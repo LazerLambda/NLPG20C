6942afb658f777e1d3d2aef6c3729e1acddb2c16
Gantari Evanda Raufani <ofinheru@gmail.com>
2019-10-29 12:27:52 +0800

    updated tokenization

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from src.SentenceSegmentation import SentenceSegmentation
 from src.AdjNounSum import PairAdjectiveNounSummarizer
 from src.utils  import load_reviews
 from src.pos_tagging import pos_tag
-from src.token_stem import ReviewTokens, CommonTokens
+from src.token_stem import Tokenization
 
 # import data
 REVIEWS = load_reviews('reviewSelected100.json')
@@ -16,8 +16,9 @@ Sentences.sentence_seg()
 Sentences.plot_sentence_length()
 
 # Tokenization and Stemming
-ReviewTokens()
-CommonTokens(REVIEWS)
+Token = Tokenization(REVIEWS)
+Token.review()
+Token.common_tokens()
 
 # POS tagging
 pos_tag(REVIEWS)
--- src/token_stem.py
+++ src/token_stem.py
@@ -12,41 +12,44 @@ stop_words = set(stopwords.words('english'))
 marks = ['(', ')', '?', '!', '.', '...', ',', '-', '$']
 ps = nltk.stem.PorterStemmer()
 
-def ReviewTokens():
-    df = pd.DataFrame(pd.read_json('reviewSelected100.json', lines=True))
+class Tokenization:
+    def __init__(self, reviews : list) -> ():
+        self.reviews = reviews
 
-    # tokens in each review
-    df['tokenizedwords'] = df.apply(lambda x: nltk.word_tokenize(x['text']), axis = 1)
-    df['lengthoftokenized'] = df.apply(lambda x: len(set(x['tokenizedwords'])), axis = 1)
-    tokencounts = df['lengthoftokenized'].value_counts().to_dict()
-    tokencounts = collections.OrderedDict(sorted(tokencounts.items(), key=lambda t: t[0]))
-    plot_graph(tokencounts, "Tokenized Reviews (without Stemming)", "No. of tokens in a review", "No. of reviews", "token_review.png")
+    def review(self):
+        df = pd.DataFrame(pd.read_json('reviewSelected100.json', lines=True))
 
-    # stems in each review
-    df['stemmedwords'] = df['tokenizedwords'].apply(lambda x: [ps.stem(y) for y in x])
-    df['lengthofstemmedtokens'] = df.apply(lambda x: len(set(x['stemmedwords'])), axis = 1)
-    stemcounts = df['lengthofstemmedtokens'].value_counts().to_dict()
-    stemcounts = collections.OrderedDict(sorted(stemcounts.items(), key=lambda t: t[0]))
-    plot_graph(stemcounts, "Tokenized Reviews (with Stemming)", "No. of tokens in a review", "No. of reviews", "token_stemmed_review.png")
+        # tokens in each review
+        df['tokenizedwords'] = df.apply(lambda x: nltk.word_tokenize(x['text']), axis = 1)
+        df['lengthoftokenized'] = df.apply(lambda x: len(set(x['tokenizedwords'])), axis = 1)
+        tokencounts = df['lengthoftokenized'].value_counts().to_dict()
+        tokencounts = collections.OrderedDict(sorted(tokencounts.items(), key=lambda t: t[0]))
+        plot_graph(tokencounts, "Tokenized Reviews (without Stemming)", "No. of tokens in a review", "No. of reviews", "token_review.png")
 
-# list of 20 most common tokens and stems
-def CommonTokens(reviews):
-    tokenlist = []
-    for review in reviews:
-        tokens = nltk.word_tokenize(review['text'])
-        for token in tokens:
-            if not (str(token).lower() in stop_words or str(token).lower() in marks):
-                tokenlist.append(token.lower())
-
-    token_total = collections.Counter(tokenlist)
-    most_common_token = token_total.most_common(20)
-    print("Most common tokens: ", most_common_token)
-
-    stemlist = []
-    for token in tokenlist:
-        if not (str(token).lower() in stop_words or str(token).lower() in marks):
-            stemlist.append(ps.stem(token).lower())
+        # stems in each review
+        df['stemmedwords'] = df['tokenizedwords'].apply(lambda x: [ps.stem(y) for y in x])
+        df['lengthofstemmedtokens'] = df.apply(lambda x: len(set(x['stemmedwords'])), axis = 1)
+        stemcounts = df['lengthofstemmedtokens'].value_counts().to_dict()
+        stemcounts = collections.OrderedDict(sorted(stemcounts.items(), key=lambda t: t[0]))
+        plot_graph(stemcounts, "Tokenized Reviews (with Stemming)", "No. of tokens in a review", "No. of reviews", "token_stemmed_review.png")
     
-    stem_total = collections.Counter(stemlist)
-    most_common_stem = stem_total.most_common(20)
-    print("Most common stems: ", most_common_stem)
+    def common_tokens(self):
+        tokenlist = []
+        for review in self.reviews:
+            tokens = nltk.word_tokenize(review['text'])
+            for token in tokens:
+                if not (str(token).lower() in stop_words or str(token).lower() in marks):
+                    tokenlist.append(token.lower())
+
+        token_total = collections.Counter(tokenlist)
+        most_common_token = token_total.most_common(20)
+        print("Most common tokens: ", most_common_token)
+
+        stemlist = []
+        for token in tokenlist:
+            if not (str(token).lower() in stop_words or str(token).lower() in marks):
+                stemlist.append(ps.stem(token).lower())
+        
+        stem_total = collections.Counter(stemlist)
+        most_common_stem = stem_total.most_common(20)
+        print("Most common stems: ", most_common_stem)
